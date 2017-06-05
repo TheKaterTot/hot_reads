3bbe618983ae8425fcd80c260a1175564fe82f42
Katy Farmer <dreadpiratekaty@gmail.com>
2017-06-05 17:06:44 -0600

    Excludes dates too far in the past

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,7 @@ class Link < ApplicationRecord
 
   def self.hot_links
     Link.select('links.url, count(links.url) as total')
+      .where('created_at > ?', 24.hours.ago)
       .group('links.url')
       .order('total desc')
       .limit(10)
